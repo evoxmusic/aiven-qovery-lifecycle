75227d334caa8a5a57e4de8008f72517f3b0da88
Romaric Philogène <rphilogene@qovery.com>
2023-02-07 10:04:02 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,19 +1,24 @@
-output "aiven_pg_service_host" {
+output "postgres_host" {
   description = "The Aiven PG connection host"
   value       = aiven_pg.pg-sample.service_host
 }
 
-output "aiven_pg_service_port" {
+output "postgres_port" {
   description = "The Aiven PG connection port"
   value       = aiven_pg.pg-sample.service_port
 }
 
-output "aiven_pg_service_username" {
+output "postgres_user" {
   description = "The Aiven PG username"
   value       = aiven_pg.pg-sample.service_username
 }
 
-output "aivenpg_service_password" {
+output "postgres_db_name" {
+  description = "The Aiven PG database name"
+  value       = aiven_pg.pg-sample.service_username
+}
+
+output "postgres_password" {
   description = "The Aiven PG password"
   value       = aiven_pg.pg-sample.service_password
   sensitive   = true
